89a6882276063352f3bd162c3d1b2dc9667185ae
raongang <erjk586@gmail.com>
2021-02-25 18:09:07 +0900

    no message

--- react_redux_egoing/src/App.js
+++ react_redux_egoing/src/App.js
@@ -3,12 +3,15 @@ import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
 class App extends Component {
+  state = { number : 0 }
+
   render() {
+    
     return (
       <div className="App">
         <h1>Root</h1>
           <AddNumberRoot></AddNumberRoot>
-          <DisplayNumberRoot></DisplayNumberRoot>
+          <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
       </div>
     )
   }
--- react_redux_egoing/src/components/DisplayNumberRoot.js
+++ react_redux_egoing/src/components/DisplayNumberRoot.js
@@ -1,11 +1,13 @@
+
 import React, {Componenet, Component} from 'react';
+import DisplayNumber from "../components/DisplayNumber";
 
 class DisplayNumberRoot extends Component{
-
     render(){
         return(
             <div>
                 <h1>Display Number Root</h1>
+                <DisplayNumber number={this.props.number}></DisplayNumber>
             </div>
 
         )
