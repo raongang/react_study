32e7b77dceb64753283a3e112ac05d019b94175d
raongang <erjk586@gmail.com>
2021-02-26 17:12:59 +0900

    no message

--- react_redux_egoing/src/App.js
+++ react_redux_egoing/src/App.js
@@ -7,6 +7,10 @@ export default class App extends Component {
 
   state = { number : 0 }
 
+  /**
+   *  AddNumber.js -> AddNumberRoot.js 자식들을 통해 넘어온 데이터(size)에 대해 부모클래스(App.js)에서 사용한다.
+   * 
+   */
   onclickHandle = (size) => {
     this.setState({ number : this.state.number + size});
   }
--- react_redux_egoing/src/containers/AddNumberRoot.js
+++ react_redux_egoing/src/containers/AddNumberRoot.js
@@ -3,6 +3,10 @@ import AddNumber from "../components/AddNumber";
 export default class AddNumberRoot extends Component{
 
 
+  /* 자식클래스인 AddNumber.js에서  this.props.onClick(this.state.size)으로 넘어왔으니
+     자식 자식클래스(AddNumberRoot.js)에서 함수를 정의하고, 부모클래스의 props에 데이터(size)를 넘긴다
+  */
+
   onclickHandle = (size) => {
    
       this.props.onClick(size);
